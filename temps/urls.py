c8c3371dd1272b2fd79d3b7ea2cea79f753b6e03
Luis Mex <lmex89@lmex89.ipv6.infinitum.net.mx>
2022-09-01 18:02:06 -0500

    commit last changes

--- temps/urls.py
+++ temps/urls.py
@@ -1,9 +1,9 @@
 from django.urls import include, path
-from temps.views import ServiceEquipmentList, TempList
+from temps.views import CpuLoadViewList, ServiceEquipmentList, TempList
 
 urlpatterns = [
      path('service_equipment_list/',
          ServiceEquipmentList.as_view()),
     path('temp_list/',TempList.as_view()),
-    path('cpu_load/',TempList.as_view()),
+    path('cpu_load/',CpuLoadViewList.as_view()),
 ]
